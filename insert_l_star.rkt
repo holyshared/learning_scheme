8548dd1808c960e8b77dd7666bbaf249eb916421
holyshared <holy.shared.design@gmail.com>
2018-01-13 17:35:31 +0900

    add insert_l_star

---
+++ insert_l_star.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((pair? (car l)) (cons (insertL* new old (car l)) (insertL* new old (cdr l))))
+      ((eq? (car l) old) (cons new (cons old (insertL* new old (cdr l) ))))
+      (else (cons (car l) (insertL* new old (cdr l))))
+    )
+))
+
+(insertL* "*" "a" '("a" "b" "c"))
+(insertL* "*" "a" '("a" "b" '("d" "e" "a") "c"))
+(insertL* "*" "a" '("a" "b" '("d" "e" '("f" "a" "g") "a") "c"))
